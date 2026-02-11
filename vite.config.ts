bcec4747fbe73a31042f6c174839b71fa59227cf
AT0MBE5T <atombest999@gmail.com>
2026-02-11 14:20:16 +0200

    offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa'
-
-const manifest: false | Partial<ManifestOptions> | undefined =
-  {
-  "theme_color": "#f4f93c",
-  "background_color": "#2EC6FE",
-  "icons": [
-    {
-      "purpose": "maskable",
-      "sizes": "512x512",
-      "src": "icon512_maskable.png",
-      "type": "image/png"
-    },
-    {
-      "purpose": "any",
-      "sizes": "512x512",
-      "src": "icon512_rounded.png",
-      "type": "image/png"
-    }
-  ],
-  screenshots:
-  [
-    {
-      src: './screenshots/desktop.png',
-      type: 'image/png',
-      sizes: '189x193',
-      form_factor: 'wide'
-    },
-    {
-      src: './screenshots/mobile.png',
-      type: 'image/png',
-      sizes: '200x42',
-      form_factor: 'narrow'
-    }
-  ],
-  "orientation": "any",
-  "display": "standalone",
-  "lang": "en-EN",
-  "name": "MyTestApp",
-  "short_name": "Testik",
-  "start_url": "/"
-};
-
-const vitePWA = VitePWA({
-  registerType: 'autoUpdate',
-  workbox: {
-    globPatterns: ["**/*.{html, css, js, ico, png, svg}"]
-  },
-  manifest: manifest
-});
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), vitePWA],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'icon512_maskable.png',
+        'icon512_rounded.png'
+      ],
+
+      manifest: {
+        theme_color: '#f4f93c',
+        background_color: '#2EC6FE',
+
+        name: 'MyTestApp',
+        short_name: 'Testik',
+
+        start_url: '/',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: 'icon512_maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'icon512_rounded.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        navigateFallback: '/index.html',
+      }
+    })
+  ]
 })
